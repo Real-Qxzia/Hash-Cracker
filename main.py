f6b97015b5398fad69b7eeebf90a139e6012b265
Real-Qxzia <167352496+Real-Qxzia@users.noreply.github.com>
2024-09-25 09:20:48 -0400

    Added sha256
    Added the ability to crack sha256 hashes

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import hashlib
 
+hash_type = input("Enter the type of hash to Crack (md5 or sha256): ").strip().lower()
 hash_to_crack = input("Enter the Hash to Crack: ").strip()
-with open('passwords.txt', 'r') as f:
-    for line in f:
-        password = line.strip()  # Strip whitespace/newline from the password
-        encrypted_password = hashlib.md5(password.encode()).hexdigest()
-        
-        if hash_to_crack == encrypted_password:
-            print(f"Successfully Cracked Password: {password}")
-            break  # Exit loop if the password is cracked
-    else:
-        # This else block executes if the loop completes without break
-        print("Hash Not Cracked")
+
+if hash_type == "md5":
+    with open('passwords.txt', 'r') as f:
+        for line in f:
+            password = line.strip()  # Strip whitespace/newline from the password
+            encrypted_password = hashlib.md5(password.encode()).hexdigest()
+
+            if hash_to_crack == encrypted_password:
+                print(f"Successfully Cracked Password: {password}")
+                break  # Exit loop if the password is cracked
+        else:
+            print("Hash Not Cracked")
+
+elif hash_type == "sha256":
+    with open('passwords.txt', 'r') as f:
+        for line in f:
+            password = line.strip()
+            encrypted_password = hashlib.sha256(password.encode()).hexdigest()
+
+            if hash_to_crack == encrypted_password:
+                print(f"Successfully cracked password {password}")
+                break
+        else:
+            print("Hash Not Cracked")
